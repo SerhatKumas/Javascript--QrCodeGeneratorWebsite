ab17d5b4a4f5e5b5ba3260124ec3955713b2c2cb
Serhat Kumas <serhat.kumas@hotmail.com>
2023-01-06 15:36:20 +0300

    Qr code dowloading and program reset is developed

--- src/QrCodeGenerator.js
+++ src/QrCodeGenerator.js
@@ -1,9 +1,10 @@
+let isThereImage = false;
 function generate(){
 let qrCodeLink = document.getElementById('TextInput').value;
 let isThereQrUrl = false;
 qrCodeLink=="" ? isThereQrUrl=false : isThereQrUrl=true;
     if(isThereQrUrl){
-        var qrcode = new QRCode(document.getElementById("qr-code"), {
+        var qrCode = new QRCode(document.getElementById("qr-code"), {
             text: qrCodeLink,
             width: 180,
             height: 180,
@@ -11,8 +12,20 @@ qrCodeLink=="" ? isThereQrUrl=false : isThereQrUrl=true;
             colorLight : "#ffffff",
             correctLevel : QRCode.CorrectLevel.H
         });
+        isThereImage = true;
+        let elem = document.getElementById("qr-code");
+        let canvas = elem.getElementsByTagName("canvas");
+        let img_src = canvas[0].toDataURL();
+        console.log(img_src);
+        document.getElementById("download-anchor").href=img_src;
     }
     else{
         document.getElementById("qr-code").innerHTML="Qr code requsted link is empty, press generate another code and don't forget to add url"
     }
+}
+
+function resetGenerator(){
+    document.getElementById('TextInput').value="";
+    document.getElementById("qr-code").innerHTML="";
+    isThereImage=false;
 }
